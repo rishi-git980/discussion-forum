3eb60fcdc024e7812139e0f180ef44441b35a7a4
Nalike Rishi <b22cn055@kitsw.ac.in>
2025-06-25 20:59:48 +0530

    Delete frontend directory

--- frontend/src/components/ui/textarea.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Form } from 'react-bootstrap';
-
-const Textarea = ({ className = '', ...props }) => {
-  return <Form.Control as="textarea" className={className} {...props} />;
-};
-
-export { Textarea }; 
